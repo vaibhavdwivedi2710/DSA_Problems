eabc7db5ce1d90ba38e522c5876d37057d768c0b
Vaibhav Dwivedi <98420486+vaibhavdwivedi2710@users.noreply.github.com>
2023-02-26 23:07:50 +0530

    Added solution - LeetHub

--- LCM And GCD - GFG/lcm-and-gcd.cpp
+++ LCM And GCD - GFG/lcm-and-gcd.cpp
@@ -9,9 +9,9 @@ class Solution {
     // helper function to get gcd of two numbers.
     long long gcd(long long a , long long b)
     {
-      if(a == 0)
-            return b;
-        return gcd(b%a, a);
+      if(b == 0)
+            return a;
+        return gcd(b,a%b);
     }
     vector<long long> lcmAndGcd(long long A , long long B) {
         
